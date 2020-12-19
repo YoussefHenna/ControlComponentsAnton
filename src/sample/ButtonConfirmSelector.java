b96dce0d6774412a4d28fe2edd2aab096062b727
Youssef Henna <youssef.hisham14@gmail.com>
2020-12-19 10:53:52 +0200

    Updated ButtonConfirmSelector with required changes

--- src/sample/ButtonConfirmSelector.java
+++ src/sample/ButtonConfirmSelector.java
@@ -26,21 +26,30 @@ public class ButtonConfirmSelector extends Region {
     private ArrayList<String> buttonValues;
     private OnButtonConfirm onButtonConfirm;
     private String currentSelected;
+    private String unselectedButtonStyle = "";
+    private String selectedButtonStyle = "";
+    private String confirmTextStyle = "";
+    private final ArrayList<StackPane> buttonStacks;
+    private final ArrayList<Button> buttons;
 
 
     /**
      * Horizontal list of buttons, with a confirm click befor action
-     * @param width width of the container
-     * @param height max height of container, to be used by selected item
-     * @param buttonValues list of values for buttons
+     *
+     * @param width           width of the container
+     * @param height          max height of container, to be used by selected item
+     * @param buttonValues    list of values for buttons
      * @param onButtonConfirm on button confirm clicked
      */
     public ButtonConfirmSelector(int width, int height, ArrayList<String> buttonValues, OnButtonConfirm onButtonConfirm) {
         this.onButtonConfirm = onButtonConfirm;
         this.buttonValues = buttonValues;
+        this.buttonStacks = new ArrayList<>();
+        this.buttons = new ArrayList<>();
         initSize(width, height);
         initUI();
         setStyle("-fx-background-color:" + bgColor);
+
     }
 
 
@@ -57,23 +66,56 @@ public class ButtonConfirmSelector extends Region {
 
     /**
      * Sets values for buttons
+     *
      * @param buttonValues list of values for buttons
      */
     public void setButtonValues(ArrayList<String> buttonValues) {
         this.buttonValues = buttonValues;
+        redraw();
     }
 
     /**
      * Sets action on confirm click
+     *
      * @param onButtonConfirm on confirm click
      */
     public void setOnButtonConfirm(OnButtonConfirm onButtonConfirm) {
         this.onButtonConfirm = onButtonConfirm;
+        redraw();
+    }
+
+    /**
+     * Sets style of confirm text
+     * @param confirmTextStyle style of confirm text in css
+     */
+    public void setConfirmTextStyle(String confirmTextStyle) {
+        this.confirmTextStyle = confirmTextStyle;
+        redraw();
+    }
+
+    /**
+     * Sets style of selected button
+     * @param selectedButtonStyle style of selected button in css
+     */
+    public void setSelectedButtonStyle(String selectedButtonStyle) {
+        this.selectedButtonStyle = selectedButtonStyle;
+        redraw();
+    }
+
+    /**
+     * Sets style of unselected button
+     * @param unselectedButtonStyle style of unselected button in css
+     */
+    public void setUnselectedButtonStyle(String unselectedButtonStyle) {
+        this.unselectedButtonStyle = unselectedButtonStyle;
+        redraw();
     }
 
     //Redraws picker -  removes all children then calls initUI
     private void redraw() {
         getChildren().clear();
+        buttons.clear();
+        buttonStacks.clear();
         initUI();
     }
 
@@ -89,20 +131,31 @@ public class ButtonConfirmSelector extends Region {
     private void initUI() {
         HBox mainContent = new HBox(2);
         mainContent.setAlignment(Pos.CENTER);
-        int btnWidthNormal = (int) ((getPrefWidth() - 20) / buttonValues.size());
-        int btnWidthBig = btnWidthNormal + 20;
+        int btnWidthNormal = (int) ((getPrefWidth()) / buttonValues.size());
+        int btnDiff = 16; //must be divisible by 2
+        int btnWidthBig = btnWidthNormal + btnDiff;
+        int btnWidthSmall = btnWidthNormal - (btnDiff / 2);
 
+        int selectedIndex = currentSelected == null ? -1 : buttonValues.indexOf(currentSelected);
         for (String s : buttonValues) {
             StackPane btnStack = new StackPane();
             btnStack.setAlignment(Pos.BOTTOM_CENTER);
+            btnStack.setPrefHeight(getPrefHeight());
+            btnStack.setMaxHeight(getPrefHeight());
             Button btn = new Button(s);
             btnStack.getChildren().add(btn);
+            buttonStacks.add(btnStack);
+            buttons.add(btn);
 
             btn.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent event) {
-                    currentSelected = s;
-                    redraw();
+                    if (currentSelected != null) {
+                        currentSelected = null;
+                    } else {
+                        currentSelected = s;
+                    }
+                    redrawUIOnly();
                 }
             });
 
@@ -113,7 +166,51 @@ public class ButtonConfirmSelector extends Region {
                 btn.setPrefHeight(getPrefHeight() - 20);
                 btn.setStyle(
                         "-fx-background-color:" + bgColor + ";" +
-                                "-fx-border-color: black;" + "-fx-font-size: 16;");
+                                "-fx-border-color: black;" + "-fx-font-size: 16;" + unselectedButtonStyle);
+
+                boolean shouldResize;
+                int currentIndex = buttonValues.indexOf(s);
+                //selected is first, should resize 2 after it
+                if (selectedIndex == 0) {
+                    shouldResize = currentIndex == selectedIndex + 1 || currentIndex == selectedIndex + 2;
+                }
+                //selected is last, should resize 2 before it
+                else if (selectedIndex == buttonValues.size() - 1) {
+                    shouldResize = currentIndex == selectedIndex - 1 || currentIndex == selectedIndex - 2;
+                }
+                //selected is in the middle, should resize 1 before it and 1 after
+                else {
+                    shouldResize = currentIndex == selectedIndex + 1 || currentIndex == selectedIndex - 1;
+                }
+                if (selectedIndex != -1 && shouldResize) {
+                    btn.setPrefWidth(btnWidthSmall);
+                    btn.setMaxWidth(btnWidthSmall);
+
+                    //current is 1 after selected, and selected is the first element
+                    if (currentIndex == selectedIndex + 1 && selectedIndex == 0) {
+                        btn.setPadding(new Insets(0, btnDiff * 1.5, 0, 0));
+                    }
+                    //current is 1 before selected, and selected is the last element
+                    else if (currentIndex == selectedIndex - 1 && selectedIndex == buttonValues.size() - 1) {
+                        btn.setPadding(new Insets(0, 0, 0, btnDiff * 1.5));
+                    }
+                    //current is 1 after selected, and selected is in the middle
+                    else if (currentIndex == selectedIndex + 1) {
+                        btn.setPadding(new Insets(0, btnDiff / 2, 0, 0));
+                    }
+                    //current is 1 before selected, and selected is in the middle
+                    else if (currentIndex == selectedIndex - 1) {
+                        btn.setPadding(new Insets(0, 0, 0, btnDiff / 2));
+                    }
+                    //current is 2 after selected
+                    else if (currentIndex == selectedIndex + 2) {
+                        btn.setPadding(new Insets(0, btnDiff / 2, 0, 0));
+                    }
+                    //current is 2 before selected
+                    else if (currentIndex == selectedIndex - 2) {
+                        btn.setPadding(new Insets(0, 0, 0, btnDiff / 2));
+                    }
+                }
 
             } else {
                 btn.setPrefWidth(btnWidthBig);
@@ -122,10 +219,10 @@ public class ButtonConfirmSelector extends Region {
                 btn.setPrefHeight(getPrefHeight());
                 btn.setStyle(
                         "-fx-background-color:" + bgColor + ";" +
-                                "-fx-border-color: black;" + "-fx-font-size: 20;" + "-fx-border-width: 3;-fx-font-weight: bold;");
+                                "-fx-border-color: black;" + "-fx-font-size: 20;" + "-fx-border-width: 3;-fx-font-weight: bold;" + selectedButtonStyle);
                 Text confirmText = new Text("Confirm");
-                confirmText.setStyle("-fx-font-size: 12;");
-                StackPane.setMargin(confirmText,new Insets(0,0,6,0));
+                confirmText.setStyle("-fx-font-size: 12;" + confirmTextStyle);
+                StackPane.setMargin(confirmText, new Insets(0, 0, 6, 0));
                 btnStack.getChildren().add(confirmText);
 
                 btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -141,5 +238,116 @@ public class ButtonConfirmSelector extends Region {
         }
 
         getChildren().add(mainContent);
+    }
+
+    //Redraws ui, without removing existing elements. Just alters them.
+    private void redrawUIOnly(){
+        int btnWidthNormal = (int) ((getPrefWidth()) / buttonValues.size());
+        int btnDiff = 16; //must be divisible by 2
+        int btnWidthBig = btnWidthNormal + btnDiff;
+        int btnWidthSmall = btnWidthNormal - (btnDiff / 2);
+
+        int selectedIndex = currentSelected == null ? -1 : buttonValues.indexOf(currentSelected);
+        for (int i = 0; i < buttonValues.size(); i++) {
+            String s = buttonValues.get(i);
+            StackPane btnStack = buttonStacks.get(i);
+
+            btnStack.getChildren().clear();//clear the stack to remove confirm text, to add later in correct button
+            Button btn = buttons.get(i);
+            btn.setPadding(new Insets(0));
+            btnStack.getChildren().add(btn);
+
+            btn.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    if (currentSelected != null) {
+                        currentSelected = null;
+                    } else {
+                        currentSelected = s;
+                    }
+                    redrawUIOnly();
+                }
+            });
+
+            //Will only execute if button wasn't already in the correct state
+            if (((currentSelected == null || !currentSelected.equals(s))) && btn.getPrefWidth() != btnWidthNormal) {
+                btn.setPrefWidth(btnWidthNormal);
+                btn.setMaxWidth(btnWidthNormal);
+                btn.setMaxHeight(getPrefHeight() - 20);
+                btn.setPrefHeight(getPrefHeight() - 20);
+                btn.setStyle(
+                        "-fx-background-color:" + bgColor + ";" +
+                                "-fx-border-color: black;" + "-fx-font-size: 16;" + unselectedButtonStyle);
+
+            } else if(btn.getPrefWidth() != btnWidthBig && currentSelected != null && currentSelected.equals(s)) {
+                btn.setPrefWidth(btnWidthBig);
+                btn.setMaxWidth(btnWidthBig);
+                btn.setMaxHeight(getPrefHeight());
+                btn.setPrefHeight(getPrefHeight());
+                btn.setStyle(
+                        "-fx-background-color:" + bgColor + ";" +
+                                "-fx-border-color: black;" + "-fx-font-size: 20;" + "-fx-border-width: 3;-fx-font-weight: bold;" + selectedButtonStyle);
+                Text confirmText = new Text("Confirm");
+                confirmText.setStyle("-fx-font-size: 12;" + confirmTextStyle);
+                StackPane.setMargin(confirmText, new Insets(0, 0, 6, 0));
+                btnStack.getChildren().add(confirmText);
+
+                btn.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        onButtonConfirm.onButtonConfirm(s);
+                    }
+                });
+
+            }
+
+            //Resizing done every time
+            if(currentSelected == null || !currentSelected.equals(s)) {
+                boolean shouldResize;
+                int currentIndex = buttonValues.indexOf(s);
+                //selected is first, should resize 2 after it
+                if (selectedIndex == 0) {
+                    shouldResize = currentIndex == selectedIndex + 1 || currentIndex == selectedIndex + 2;
+                }
+                //selected is last, should resize 2 before it
+                else if (selectedIndex == buttonValues.size() - 1) {
+                    shouldResize = currentIndex == selectedIndex - 1 || currentIndex == selectedIndex - 2;
+                }
+                //selected is in the middle, should resize 1 before it and 1 after
+                else {
+                    shouldResize = currentIndex == selectedIndex + 1 || currentIndex == selectedIndex - 1;
+                }
+                if (selectedIndex != -1 && shouldResize) {
+                    btn.setPrefWidth(btnWidthSmall);
+                    btn.setMaxWidth(btnWidthSmall);
+
+                    //current is 1 after selected, and selected is the first element
+                    if (currentIndex == selectedIndex + 1 && selectedIndex == 0) {
+                        btn.setPadding(new Insets(0, btnDiff * 1.5, 0, 0));
+                    }
+                    //current is 1 before selected, and selected is the last element
+                    else if (currentIndex == selectedIndex - 1 && selectedIndex == buttonValues.size() - 1) {
+                        btn.setPadding(new Insets(0, 0, 0, btnDiff * 1.5));
+                    }
+                    //current is 1 after selected, and selected is in the middle
+                    else if (currentIndex == selectedIndex + 1) {
+                        btn.setPadding(new Insets(0, btnDiff / 2, 0, 0));
+                    }
+                    //current is 1 before selected, and selected is in the middle
+                    else if (currentIndex == selectedIndex - 1) {
+                        btn.setPadding(new Insets(0, 0, 0, btnDiff / 2));
+                    }
+                    //current is 2 after selected
+                    else if (currentIndex == selectedIndex + 2) {
+                        btn.setPadding(new Insets(0, btnDiff / 2, 0, 0));
+                    }
+                    //current is 2 before selected
+                    else if (currentIndex == selectedIndex - 2) {
+                        btn.setPadding(new Insets(0, 0, 0, btnDiff / 2));
+                    }
+                }
+            }
+
+        }
     }
 }
